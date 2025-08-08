62cb9ecc8e20e670984fed51e37d1dee494af251
Mohit Narnaware <mohitnarnaware48@gmail.com>
2025-08-08 16:13:47 +0530

    otp4

---
+++ backend/migrations/versions/2b6a78668c7a_merge_branches_into_single_head.py
@@ -0,0 +1,24 @@
+"""merge branches into single head
+
+Revision ID: 2b6a78668c7a
+Revises: 28097d44b1e8
+Create Date: 2025-08-08 15:59:22.683794
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2b6a78668c7a'
+down_revision = '28097d44b1e8'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
